65f1037b2b2d673eed8d0915afb40ae722b75f1d
tomwang <174604588@qq.com>
2016-04-22 23:50:19 +0800

    add 2 models

---
+++ src/models/student.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+let Schema = mongoose.Schema;
+let studentSchema = Schema({
+  xingMing:     String, // 姓名
+  schoolName:   String, // 学校名
+  major:        String, // 专业
+  entranceTime: Date,   // 入学时间
+  gender:       Number, // 性别：0:female,1:male
+  _userId:       Schema.Types.ObjectId
+});
+
+let Student = mongoose.model('Student', userSchema);
+
+export default Student;
+
--- src/models/user.js
+++ src/models/user.js
@@ -1,8 +1,12 @@
 import mongoose from 'mongoose';
 
-let userSchema = mongoose.Schema({
-  email: String,
-  password: String
+let Schema = mongoose.Schema;
+let userSchema = Schema({
+  name:     String, // 用户名
+  email:    String, // 邮箱
+  phone:    String, // 电话号码
+  password: String, // 加密后的密码
+  cate:     Number  // 用户类型：0:个人,1:企业
 });
 
 let User = mongoose.model('User', userSchema);
