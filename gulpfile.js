26324cc0b640d43e05a6e164d658a64a567171c5
tomwang <174604588@qq.com>
2016-07-10 22:40:58 +0800

    add js gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,34 @@
 var gulp        = require('gulp');
-var babel       = require('gulp-babel');
 var browserify  = require('browserify');
-var babelify    = require('babelify');
 var source      = require('vinyl-source-stream');
-var sass        = require('gulp-sass');
-var concat      = require('gulp-concat');
 var uglify      = require('gulp-uglify');
-var streamify   = require('gulp-streamify');
+var buffer      = require('vinyl-buffer');
+var sourcemaps  = require('gulp-sourcemaps');
+
+gulp.task('vendor.js', function() {
+  return browserify()
+    .require(['jquery', './assets/js/common.js'])
+    .bundle()
+    .pipe(source('vendor.js'))
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(gulp.dest('./public/js/'));
+});
+
+gulp.task('bundle.js', function() {
+  return browserify({
+    entries: './assets/js/main.js',
+    debug:   true
+  }).external(['jquery', 'common'])
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./public/js/'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./assets/js/**/*.js', ['bundle.js']);
+});
