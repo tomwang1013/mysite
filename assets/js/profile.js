86b88c577b324518044bdc78f0e0b6943cca83f6
wangxiantong <wangxiantong@cd.tuan800.com>
2016-07-22 12:50:09 +0800

    more

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -2,17 +2,25 @@ var $ = require('jquery');
 
 $('.profile-right :button').click(function() {
   var inp = $(this).prev();
+  var hint = $(this).next();
   var readonly = inp.prop('readonly');
 
   if (readonly) { inp.removeClass('noneditable').prop('readonly', false).focus();
+    // 点击修改
     $(this).val('保存');
   } else {
-    var param = {};
-    param[inp.attr('name')] = inp.val();
-
-    $('./profile/change_user_info', { attrName: inp.attr('name'), value: inp.val() }, function(data) {
+    // 点击保存
+    $.post('./profile/change_user_info', {
+      attrName: inp.attr('name'),
+      value:    inp.val()
+    }, function(data) {
       if (data.error) {
+        hint.addClass('hint-error').text(data.message).show();
+        inp.focus();
       } else {
+        inp.addClass('noneditable').prop('readonly', true);
+        hint.addClass('hint-ok').text('保存成功').fadeOut();
+        $(this).val('修改');
       }
     });
   }
