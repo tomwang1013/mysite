66f6da57ff5634f0e5dd3943bc0dfe1837306852
wangxiantong <wangxiantong@cd.tuan800.com>
2016-08-12 13:29:51 +0800

    major crawler over

--- models/major.js
+++ models/major.js
@@ -9,7 +9,7 @@ let Schema = mongoose.Schema;
 let majorSchema = Schema({
   name:       String,   // 专业名称
   type:       String,   // 类别(工学，文学，...)
-  eduRank:    Number,   // 学历层次: 1 本科 2 专科
+  eduRank:    String,   // 学历层次: 本科 专科
 });
 
 let Major = mongoose.model('Major', majorSchema);
