10dbc2167892c978dae9d2af6f1be29219b10339
wangxiantong <wangxiantong@cd.tuan800.com>
2016-11-11 13:33:25 +0800

    more

--- assets/js/common/index.js
+++ assets/js/common/index.js
@@ -2,3 +2,4 @@ require('./serialize_object');
 require('./popup_tabs');
 require('./popup_list');
 require('./validation_defaults')
+require('./site')
--- assets/js/common/site.js
+++ assets/js/common/site.js
@@ -5,7 +5,23 @@
 var $ = require('jquery');
 
 $(function() {
+  var dropdownTimer;
+
   $('.profile-index').on('mouseenter', function() {
     $('.profile-dropdown').fadeIn();
+    dropdownTimer = setTimeout(function() {
+      $('.profile-dropdown').fadeOut();
+    }, 1000);
+  });
+
+  $('.profile-dropdown').on('mouseenter', function() {
+    if (dropdownTimer) {
+      clearTimeout(dropdownTimer);
+      dropdownTimer = null;
+    }
+  });
+
+  $('.profile-dropdown').on('mouseleave', function() {
+    $(this).fadeOut();
   });
 });
