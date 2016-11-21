dc0d83ead937d0a0bd1c43d2d237348bfcf5755c
tomwang <174604588@qq.com>
2016-11-22 03:57:53 +0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -10,29 +10,22 @@ var sass        = require('gulp-sass');
 var glob        = require('glob');
 var es          = require('event-stream');
 var rev         = require('gulp-rev');
+var cleanCSS    = require('gulp-clean-css');
 
 var deps = ['jquery', 'lodash'];
 
 gulp.task('vendor.js', function() {
   return browserify({
     entries: [
-    './node_modules/jquery-validation/dist/jquery.validate.js',
-    './node_modules/jquery-validation/dist/additional-methods.js',
-    './node_modules/jquery-validation/dist/localization/messages_zh.js',
-    './assets/js/common']
+    'node_modules/jquery-validation/dist/jquery.validate.js',
+    'node_modules/jquery-validation/dist/additional-methods.js',
+    'node_modules/jquery-validation/dist/localization/messages_zh.js',
+    'assets/js/common'],
+    debug: true
   }).require(deps)
     .bundle()
     .pipe(source('vendor.js'))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({ loadMaps: true }))
-    .pipe(rev())
-    .pipe(uglify())
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./public/js'))
-    .pipe(rev.manifest({
-      base: './public',
-      merge: true
-    }));
+    .pipe(gulp.dest('public/js'));
 });
 
 gulp.task('bundle.js', function(done) {
@@ -49,10 +42,6 @@ gulp.task('bundle.js', function(done) {
 
       return b.external(deps).bundle()
       .pipe(source(entry.slice(entry.lastIndexOf('/') + 1)))
-      .pipe(buffer())
-      .pipe(sourcemaps.init({ loadMaps: true }))
-      .pipe(uglify())
-      .pipe(sourcemaps.write('./'))
       .pipe(gulp.dest('public/js'));
     });
 
@@ -61,19 +50,43 @@ gulp.task('bundle.js', function(done) {
 });
 
 gulp.task('sass', function () {
-  return gulp.src('./assets/sass/**/[^_]*.scss')
+  return gulp.src(['assets/sass/**/[^_]*.scss', '!assets/sass/font-awesome/font-awesome.scss'])
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./public/css/'));
+    .pipe(gulp.dest('public/css/'));
 });
 
 gulp.task('font-awesome', function () {
-  return gulp.src('./assets/sass/font-awesome/font-awesome.scss')
+  return gulp.src('assets/sass/font-awesome/font-awesome.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./public/css/'));
+    .pipe(gulp.dest('public/css/'));
+});
+
+gulp.task('manifest-js', ['vendor.js', 'bundle.js'], function() {
+  gulp.src(['public/js/+([^-]).js'], { base: 'public' })
+    .pipe(buffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(uglify())
+    .pipe(rev())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('public'))
+    .pipe(rev.manifest('rev-manifest.json', { merge: true }))
+    .pipe(gulp.dest('.'));
+});
+
+gulp.task('manifest-css', ['sass', 'font-awesome'], function() {
+  gulp.src(['public/css/+([^-]).css'], { base: 'public' })
+    .pipe(buffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(cleanCSS())
+    .pipe(rev())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('public'))
+    .pipe(rev.manifest('rev-manifest.json', { merge: true }))
+    .pipe(gulp.dest('.'));
 });
 
 gulp.task('watch', function() {
-  gulp.watch('./assets/sass/**/*.scss', ['sass']);
-  gulp.watch('./assets/js/common/*.js', ['vendor.js']);
-  gulp.watch('./assets/js/*.js', ['bundle.js']);
+  gulp.watch('assets/sass/**/*.scss', ['sass']);
+  gulp.watch('assets/js/common/*.js', ['vendor.js']);
+  gulp.watch('assets/js/*.js', ['bundle.js']);
 });
