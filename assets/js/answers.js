8537970f41be7c1b39bfcd2312f6052614f2913f
wangxiantong <wangxiantong@cd.tuan800.com>
2016-12-27 10:31:10 +0800

    完善解答列表页ui功能

--- assets/js/answers.js
+++ assets/js/answers.js
@@ -1,26 +1,24 @@
 var $ = require('jquery');
 
 $(function() {
+  // 企业给解答评分
   $('.a-to-score, .a-re-score').click(function() {
+    var a = $(this).closest('.answer');
+
     $('.overlay').show();
     $('.overlay-content').show();
-    $('#score').focus().val($('#score').val());
+
+    $('.set-score').attr('action',
+                         '/question/' + a.data('qid') +
+                           '/answer/' + a.data('aid') + '/update_score');
+    $('#score').focus().val(a.data('score'));
+    $('#comment').val(a.data('comment'));
     return false;
   });
 
   $('.set-score .cancel').click(function() {
     $('.overlay').hide();
     $('.overlay-content').hide();
-  });
-
-  $('.del-answer').click(function() {
-    if (confirm('确定要删除此解答吗？')) {
-      $.post($(this).data('link'), {
-        job_id: $(this).data('jobId')
-      }, function(data) {
-        location.replace(data.location);
-      });
-    }
   });
 
   $('.set-score').submit(function() {
@@ -31,5 +29,16 @@ $(function() {
     });
 
     return false;
+  });
+
+  // 学生删除解答
+  $('.del-answer').click(function() {
+    if (confirm('确定要删除此解答吗？')) {
+      $.post($(this).data('link'), {
+        job_id: $(this).data('jobId')
+      }, function(data) {
+        location.replace(data.location);
+      });
+    }
   });
 });
