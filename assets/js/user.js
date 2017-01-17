f03bc0221f23cff6e528760cd7fa074916bf947f
tomwang <174604588@qq.com>
2017-01-17 17:35:36 +0800

    css refactoring init over

--- assets/js/user.js
+++ assets/js/user.js
@@ -5,11 +5,11 @@ var z = require('common/popup_tabs');
 var w = require('common/main_nav');
 
 $(document).ready(function() {
-  $('#university').popupList({ remoteUrl: '/universities' });
+  $('#university').popupList({remoteUrl: '/universities' });
   $('#major').popupTabs(window.ms);
 
   // 用户注册第一步
-  $('form#signup-step1').validate({
+  $('#signup-step1').validate({
     rules: {
       name: {
         remote: '/signup/is_valid_name'
@@ -22,6 +22,8 @@ $(document).ready(function() {
   });
 
   // 用户注册第二步
-  $('form#signup-step2').validate();
-  $('form.login').validate();
+  $('#signup-step2').validate();
+
+  // 登陆
+  $('.js-login-fm').validate();
 });
