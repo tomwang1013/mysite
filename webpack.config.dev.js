d1b974d10e1cea31f1bccaa6bad6cdf5e4e3ca19
wangxiantong <wangxiantong@cd.tuan800.com>
2017-01-19 10:57:31 +0800

    add extract plugin

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,5 +1,18 @@
-const baseConfig = require('./webpack.config.base.js');
-const webpackMerge = require('webpack-merge');
+const baseConfig        = require('./webpack.config.base.js');
+const webpackMerge      = require('webpack-merge');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = webpackMerge(baseConfig, {
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  },
+
+  plugins: [
+    new ExtractTextPlugin({ filename: '[name].css' })
+  ]
 });
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,15 +1,29 @@
-const baseConfig     = require('./webpack.config.base.js');
-const webpackMerge   = require('webpack-merge');
-const WebpackMd5Hash = require('webpack-md5-hash');
-const ManifestPlugin = require('webpack-manifest-plugin');
+const baseConfig        = require('./webpack.config.base.js');
+const webpackMerge      = require('webpack-merge');
+const WebpackMd5Hash    = require('webpack-md5-hash');
+const ManifestPlugin    = require('webpack-manifest-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = webpackMerge(baseConfig, {
   output: {
     filename: '[name].[chunkhash].js'
   },
 
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract({
+          fallbackLoader: "style-loader",
+          loader: ["css-loader", "sass-loader"]
+        })
+      }
+    ]
+  },
+
   plugins: [
     new WebpackMd5Hash(),
     new ManifestPlugin({ fileName: 'rev-manifest.json' })
+    new ExtractTextPlugin({ filename: '[name].[contenthash].css' })
   ]
 });
