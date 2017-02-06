b7999d502b6e8c90434bd48936c18c444d33be17
wangxiantong <wangxiantong@cd.tuan800.com>
2017-02-06 13:51:21 +0800

    almost work

--- assets/js/common/main_nav.js
+++ assets/js/common/main_nav.js
@@ -8,32 +8,32 @@ $(function() {
   // header
   var dropdownTimer;
 
-  $('.profile-index').on('mouseenter', function() {
-    $('.profile-dropdown').fadeIn();
+  $('.js-show-menu').on('mouseenter', function() {
+    $('.js-profile-dropdown').fadeIn();
     dropdownTimer = setTimeout(function() {
-      $('.profile-dropdown').fadeOut();
+      $('.js-profile-dropdown').fadeOut();
     }, 1000);
   });
 
-  $('.profile-dropdown').on('mouseenter', function() {
+  $('.js-profile-dropdown').on('mouseenter', function() {
     if (dropdownTimer) {
       clearTimeout(dropdownTimer);
       dropdownTimer = null;
     }
   });
 
-  $('.profile-dropdown').on('mouseleave', function() {
+  $('.js-profile-dropdown').on('mouseleave', function() {
     $(this).fadeOut();
   });
 
   // check if has unread message
   $.get('/profile/message_status', function(data) {
     if (data.has_msg) {
-      $('.msg-notify').addClass('has-msg');
-      $('.msg-tip').text('你有未读消息');
+      $('.js-msg-notify').addClass('has-msg');
+      $('.js-msg-tip').text('你有未读消息');
     } else {
-      $('.msg-notify').removeClass('has-msg');
-      $('.msg-tip').text('你暂时没有未读消息');
+      $('.js-msg-notify').removeClass('has-msg');
+      $('.js-msg-tip').text('你暂时没有未读消息');
     }
   });
 });
