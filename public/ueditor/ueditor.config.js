4b6463ff8d9df5b92bbf054aa3474f70cd4dddee
wangxiantong <wangxiantong@cd.tuan800.com>
2017-02-14 14:34:27 +0800

    more

--- public/ueditor/ueditor.config.js
+++ public/ueditor/ueditor.config.js
@@ -30,7 +30,7 @@
         UEDITOR_HOME_URL: URL
 
         // 服务器统一请求接口路径
-        , serverUrl: "/ueditor/config"  // 统一在nodejs中处理所有服务端请求
+        , serverUrl: "/ueditor"  // 统一在nodejs中处理所有服务端请求
 
         //工具栏上的所有的功能按钮和下拉框，可以在new编辑器的实例时选择自己需要的重新定义
         , toolbars: [[
