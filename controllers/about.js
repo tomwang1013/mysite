283a3b0c964f67efa25ef6ca57630baf6d4057c0
wangxiantong <wangxiantong@cd.tuan800.com>
2017-04-06 13:49:02 +0800

    add validator test

--- controllers/about.js
+++ controllers/about.js
@@ -2,4 +2,12 @@ function index(req, res, next) {
   res.render('about/index');
 };
 
-exports = module.exports = { index };
+function fmValTest(req, res, next) {
+  res.render('about/fm_val_test');
+}
+
+function something(req, res, next) {
+  res.json({ error: 0 });
+}
+
+exports = module.exports = { index, fmValTest, something };
